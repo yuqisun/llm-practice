38b5099586bdd4baa8cbcaa6dd01b405f2fed880
Yuqi Sun <791079835@qq.com>
2024-05-19 22:31:11 +0800

    chroma usage guide

--- chroma/usage_guide.py
+++ chroma/usage_guide.py
@@ -3,6 +3,10 @@ import chromadb
 # chroma中自定义Embeddings的几种方法
 # https://zhuanlan.zhihu.com/p/649413704
 
+# onnx.tar.gz
+# https://developer.aliyun.com/article/1464026
+# https://chroma-onnx-models.s3.amazonaws.com/all-MiniLM-L6-v2/onnx.tar.gz
+
 # python can also run in-memory with no server running: chromadb.PersistentClient()
 client = chromadb.PersistentClient()
 collection = client.get_or_create_collection(name="my_collection")
