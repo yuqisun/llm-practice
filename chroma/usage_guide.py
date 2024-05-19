905f8f94ecd14a01938b08e66587e5d56de01d38
Yuqi Sun <791079835@qq.com>
2024-05-19 22:26:42 +0800

    chroma usage guide

---
+++ chroma/usage_guide.py
@@ -0,0 +1,19 @@
+import chromadb
+
+# chroma中自定义Embeddings的几种方法
+# https://zhuanlan.zhihu.com/p/649413704
+
+# python can also run in-memory with no server running: chromadb.PersistentClient()
+client = chromadb.PersistentClient()
+collection = client.get_or_create_collection(name="my_collection")
+col_cnt = collection.count()
+print(f"Collection count: {col_cnt}")
+
+collection.add(
+    documents=["lorem ipsum...", "doc2", "doc3"],
+    metadatas=[{"chapter": "3", "verse": "16"}, {"chapter": "3", "verse": "5"}, {"chapter": "29", "verse": "11"}],
+    ids=["id1", "id2", "id3"]
+)
+col_cnt = collection.count()
+print(f"Collection count: {col_cnt}")
+
